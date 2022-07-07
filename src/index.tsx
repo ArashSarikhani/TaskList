0847d3638069dcdbcf9e66760cfea28c42674b83
Arash Sarikhani <arsa917@gmail.com>
2022-07-07 18:51:11 +0430

    Create a Redux Store

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
+import { store } from "./store";
+import { Provider } from "react-redux";
 const container = document.getElementById("tlpwa");
 const root = createRoot(container!);
 
 root.render(
-  <>
+  <Provider store={store}>
     <App />
-  </>
+  </Provider>
 );
 
 serviceWorkerRegistration.register();
